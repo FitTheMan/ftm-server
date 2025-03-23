25b106d5a43102330a03027b667b640bb71cb8cb
songhyeonpk <ehftozl1997@gmail.com>
2025-03-23 14:33:38 +0900

    feat: UseCase, Infrastructure 계층 커스텀 어노테이션 추가

--- src/main/java/com/ftm/server/infrastructure/redis/RedisService.java
+++ src/main/java/com/ftm/server/infrastructure/redis/RedisService.java
@@ -2,12 +2,12 @@ package com.ftm.server.infrastructure.redis;
 
 import com.ftm.server.adapter.gateway.RedisCacheGateway;
 import com.ftm.server.adapter.gateway.RedisSessionGateway;
+import com.ftm.server.common.annotation.InfraService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.stereotype.Service;
 
 /** Redis Caching, Session 구현체 각 역할 별 레디스 조작 관리 (비즈니스 로직이 포함되면 안됨, 기술적인 로직만 수행) */
-@Service
+@InfraService
 @RequiredArgsConstructor
 public class RedisService implements RedisCacheGateway, RedisSessionGateway {
 
