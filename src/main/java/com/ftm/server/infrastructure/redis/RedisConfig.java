1457cefc682b2f0972b018f4d5e292c83aa1fe1d
songhyeonpk <ehftozl1997@gmail.com>
2025-04-10 16:43:55 +0900

    refactor: redis 캐시 관련 설정 삭제

--- src/main/java/com/ftm/server/infrastructure/redis/RedisConfig.java
+++ src/main/java/com/ftm/server/infrastructure/redis/RedisConfig.java
@@ -1,22 +1,16 @@
 package com.ftm.server.infrastructure.redis;
 
-import java.time.Duration;
 import lombok.RequiredArgsConstructor;
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.cache.RedisCacheConfiguration;
-import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
-import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @EnableRedisHttpSession(redisNamespace = "ftm:session", maxInactiveIntervalInSeconds = 3600)
-@EnableCaching
 @Configuration
 @RequiredArgsConstructor
 public class RedisConfig {
@@ -45,21 +39,5 @@ public class RedisConfig {
 
         template.afterPropertiesSet();
         return template;
-    }
-
-    // Redis Caching
-    @Bean
-    public RedisCacheManager redisCacheManager(RedisConnectionFactory factory) {
-        RedisCacheConfiguration cacheConfiguration =
-                RedisCacheConfiguration.defaultCacheConfig()
-                        .entryTtl(Duration.ofMinutes(30)) // 기본 캐시 만료시간 30분
-                        .serializeKeysWith(
-                                RedisSerializationContext.SerializationPair.fromSerializer(
-                                        new StringRedisSerializer()))
-                        .serializeValuesWith(
-                                RedisSerializationContext.SerializationPair.fromSerializer(
-                                        new GenericJackson2JsonRedisSerializer()));
-
-        return RedisCacheManager.builder(factory).cacheDefaults(cacheConfiguration).build();
     }
 }
