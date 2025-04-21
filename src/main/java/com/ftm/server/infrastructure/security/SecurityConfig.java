88b70ecd8ba959bca46f7125ec129793c5103e72
myqewr <mygongjoo@gmail.com>
2025-04-21 21:38:42 +0900

    feat : 세션 유효성 검사 api 추가(#102)

--- src/main/java/com/ftm/server/infrastructure/security/SecurityConfig.java
+++ src/main/java/com/ftm/server/infrastructure/security/SecurityConfig.java
@@ -51,7 +51,10 @@ public class SecurityConfig {
                     "https://fittheman.site"); // 개발 환경 클라이언트 도메인
 
     private static final String[] GET_ANONYMOUS_MATCHERS = {
-        "/api/users/email/duplication", "/api/users/options", "/api/grooming/tests"
+        "/api/users/email/duplication",
+        "/api/users/options",
+        "/api/grooming/tests",
+        "/api/auth/session/validity"
     };
 
     private static final String[] POST_ANONYMOUS_MATCHERS = {
