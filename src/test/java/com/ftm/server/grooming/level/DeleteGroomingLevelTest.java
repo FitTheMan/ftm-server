f0b73d18910338a1ac1c13c6dc7acb3dac75f5df
songhyeonpk <ehftozl1997@gmail.com>
2025-05-31 13:25:22 +0900

    test: 관리자 그루밍 레벨 생성, 수정, 삭제 테스트 추가

---
+++ src/test/java/com/ftm/server/grooming/level/DeleteGroomingLevelTest.java
@@ -0,0 +1,63 @@
+package com.ftm.server.grooming.level;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.ftm.server.BaseTest;
+import com.ftm.server.adapter.out.persistence.mapper.GroomingLevelMapper;
+import com.ftm.server.adapter.out.persistence.repository.GroomingLevelRepository;
+import com.ftm.server.application.command.grooming.level.SaveGroomingLevelCommand;
+import com.ftm.server.common.response.enums.ErrorResponseCode;
+import com.ftm.server.domain.entity.GroomingLevel;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mock.web.MockHttpSession;
+import org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.transaction.annotation.Transactional;
+
+public class DeleteGroomingLevelTest extends BaseTest {
+
+    @Autowired private GroomingLevelRepository groomingLevelRepository;
+    @Autowired private GroomingLevelMapper groomingLevelMapper;
+
+    private Long levelId;
+
+    private ResultActions getResultActions(Long levelId, MockHttpSession session) throws Exception {
+        return mockMvc.perform(
+                RestDocumentationRequestBuilders.delete("/api/grooming/levels/{levelId}", levelId)
+                        .session(session));
+    }
+
+    @BeforeEach
+    void setUp() {
+        SaveGroomingLevelCommand command = SaveGroomingLevelCommand.of(0, 10, "테스트 순한맛", "테스트 매운맛");
+        GroomingLevel level = GroomingLevel.create(command);
+        levelId = groomingLevelRepository.save(groomingLevelMapper.toJpaEntity(level)).getId();
+    }
+
+    @Test
+    @Transactional
+    void 그루밍_레벨_삭제_성공() throws Exception {
+        // when
+        MockHttpSession session = createAdminUserAndLogin();
+        ResultActions resultActions = getResultActions(levelId, session);
+
+        // given
+        resultActions.andExpect(status().isOk()).andDo(print());
+    }
+
+    @Test
+    @Transactional
+    void 그루밍_레벨_삭제_실패() throws Exception {
+        // when
+        MockHttpSession session = createUserAndLogin();
+        ResultActions resultActions = getResultActions(levelId, session);
+
+        // given
+        resultActions
+                .andExpect(status().is(ErrorResponseCode.NOT_AUTHORIZATION.getHttpStatus().value()))
+                .andDo(print());
+    }
+}
