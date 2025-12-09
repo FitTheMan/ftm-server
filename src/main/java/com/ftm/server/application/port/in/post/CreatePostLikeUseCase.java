998f8a4a7f54d0de4067b4d100cb7a605607b643
myqewr <mygongjoo@gmail.com>
2025-12-09 21:19:51 +0900

    feature : 게시글 좋아요 api + 상세 게시글 유저 좋아요 여부 필드 추가 - 철타 오타 수정(#171)

--- src/main/java/com/ftm/server/application/port/in/post/CreatePostLikeUseCase.java
+++ src/main/java/com/ftm/server/application/port/in/post/CreatePostLikeUseCase.java
@@ -3,6 +3,6 @@ package com.ftm.server.application.port.in.post;
 import com.ftm.server.common.annotation.UseCase;
 
 @UseCase
-public interface CreatePostLikeUserCase {
+public interface CreatePostLikeUseCase {
     Boolean execute(Long userId, Long postId);
 }
